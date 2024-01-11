1d25ebfecf8c3632098b8b5fa997862d51910014
alijr2018 <alijr_2018@proton.me>
2024-01-11 14:54:44 +0100

    new

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+import math
+
+
+def to_str(n: float) -> str:
+    """
+    Write a type-annotated function to_str that takes a float,
+    n as argument and returns the string representation,
+    of the float.
+    """
+    return str(n)
---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python3
+
+def sum_list(i)
