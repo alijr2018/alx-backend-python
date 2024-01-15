1150a9883f61424f434fda6cecd1ef261c15cc2e
alijr2018 <alijr_2018@proton.me>
2024-01-15 13:40:49 +0100

    check

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -8,7 +8,7 @@ import random
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int = 10) -> List[float]:
+async def wait_n(n: int, max_delay: int) -> List[float]:
     """
     Import wait_random from the previous python file,
     that you’ve written and write an async routine,
