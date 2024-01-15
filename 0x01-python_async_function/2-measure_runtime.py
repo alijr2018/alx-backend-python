0eebbc2c8524b6d11f41204181eb45a9b3d027e8
alijr2018 <alijr_2018@proton.me>
2024-01-15 13:27:11 +0100

    done

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -7,6 +7,7 @@ from typing import List
 import time
 wait_n = __import__('1-concurrent_coroutines').wait_n
 
+
 def measure_time(n: int, max_delay: int) -> float:
     """
     Create a measure_time function with integers,
