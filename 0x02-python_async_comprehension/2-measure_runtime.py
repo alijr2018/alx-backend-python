188b0768b06bf95788c57543c979397d315f737e
alijr2018 <alijr_2018@proton.me>
2024-01-16 20:17:16 +0100

    ??

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -13,12 +13,8 @@ async def measure_runtime() -> float:
     """
     s_time = asyncio.get_event_loop().time()
 
-    await asyncio.gather(
-        async_comprehension(),
-        async_comprehension(),
-        async_comprehension(),
-        async_comprehension()
-    )
+    await asyncio.gather(async_comprehension(), async_comprehension(),
+            async_comprehension(), async_comprehension())
 
     e_time = asyncio.get_event_loop().time()
 
