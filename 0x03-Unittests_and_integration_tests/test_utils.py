976dc3074745dba004f069a9a445342c7bcf1ab1
alijr2018 <alijr_2018@proton.me>
2024-02-04 11:59:41 +0100

    fix

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -19,6 +19,15 @@ class TestAccessNestedMap(unittest.TestCase):
         self.assertEqual(
             utils.access_nested_map(nested_map, path), expected_result)
 
+    @parameterized.expand([
+        ({}, ("a",), KeyError("Key not found: 'a' in {}")),
+        ({"a": 1}, ("a", "b"), KeyError("Key not found: 'b' in 1")),
+    ])
+    def test_access_nested_map_exception(self, nested_map, path):
+        with self.assertRaises(KeyError) as error:
+            utils.access_nested_map(nested_map, path)
+        self.assertEqual(error.exception.args[0], path[-1])
+
 
 if __name__ == '__main__':
     unittest.main()
