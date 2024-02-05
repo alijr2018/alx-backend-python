88702423d6d6799db8124a71a34546af80868b22
alijr2018 <alijr_2018@proton.me>
2024-02-05 12:53:24 +0100

    6. More patching

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -44,6 +44,26 @@ class TestGithubOrgClient(unittest.TestCase):
         expected_url = 'https://api.github.com/orgs/mocked_org/repos'
         self.assertEqual(gh_org_client._public_repos_url, expected_url)
 
+    @patch("client.get_json",
+           return_value=[{"name": "repo1"}, {"name": "repo2"}])
+    @patch("client.GithubOrgClient._public_repos_url",
+           new_callable=PropertyMock)
+    def test_public_repos(self, mock_repos_url, mock_get_json):
+        mock_repos_url.return_value = "https://api.github.com/orgs/mocked_org/repos"
+
+        gh_org_client = GithubOrgClient("mocked_org")
+
+        repos = gh_org_client.public_repos()
+
+        expected_repos = ["repo1", "repo2"]
+
+        self.assertEqual(repos, expected_repos)
+
+        mock_repos_url.assert_called_once()
+
+        mock_get_json.assert_called_once_with(
+            "https://api.github.com/orgs/mocked_org/repos")
+
 
 if __name__ == "__main__":
     unittest.main()
