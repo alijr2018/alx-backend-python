88d34174ed59f2bcc65741320c68ccfce3710cbf
alijr2018 <alijr_2018@proton.me>
2024-02-05 19:29:22 +0100

    7. Parameterize

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -68,6 +68,17 @@ class TestGithubOrgClient(unittest.TestCase):
         mock_get_json.assert_called_once_with(
             "https://api.github.com/orgs/mocked_org/repos")
 
+    @parameterized.expand([
+        ({"license": {"key": "my_license"}}, "my_license", True),
+        ({"license": {"key": "other_license"}}, "my_license", False),
+    ])
+    @patch("client.GithubOrgClient.org")
+    def test_has_license(self, repo, license_key, expected_result, mock_org):
+        mock_org.return_value = {"repos_url": "mocked_url"}
+        github_client = GithubOrgClient("mocked_org")
+        result = github_client.has_license(repo, license_key)
+        self.assertEqual(result, expected_result)
+
 
 if __name__ == "__main__":
     unittest.main()
