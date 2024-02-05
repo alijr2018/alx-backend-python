93f6e4a39595908dd3707d6eaca73627139d249c
alijr2018 <alijr_2018@proton.me>
2024-02-05 10:46:17 +0100

    ?

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -56,7 +56,7 @@ class TestMemoize(unittest.TestCase):
         def a_method(self):
             return 42
 
-        @utils.memoize
+        @memoize
         def a_property(self):
             return self.a_method()
 
